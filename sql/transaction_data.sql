be542309deef3718dcd6f3b889463bde796a62d5
karan8891 <55722488+karan8891@users.noreply.github.com>
2024-08-09 20:39:04 -0400

    Create transaction_data.sql

---
+++ sql/transaction_data.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `genial-upgrade-427514-r9.your_dataset.transaction_data` (
+  user_id STRING,
+  user_name STRING,
+  user_email STRING,
+  product_id STRING,
+  product_name STRING,
+  product_category STRING,
+  transaction_amount FLOAT,
+  transaction_time TIMESTAMP
+);
